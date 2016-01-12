2d1b3aaa3f8e1b7e255488078b835fc5b6f0f224
Chad Oehlschlaeger-Browne <chadnobrowne@gmail.com>
2016-01-11 17:07:37 -0800

    fingers crossed

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ var express = require('express');
 var app = express();
 var bodyparser = require('body-parser');
 var port = process.env.PORT || 3002;
-app.use(express.static(__dirname + '/app/'))
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({
   extended: true
 }));
-
+app.use(express.static(__dirname + '/app/'));
 app.get('/', function(req, res) {
   res.sendFile('index.html');
+});
+app.listen(port, function() {
+  console.log('server starting. available at http://localhost:' + port);
 });
